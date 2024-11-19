021647f28bf27781c985c4399d990324675b9b92
Shubh Gupta <shubhorai12@gmail.com>
2024-11-19 17:10:58 +0530

    Enhance greeting response to include user's name from query parameters

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, resp)=>{
-    resp.send("Hello World");
+    console.log("data sent by browser = ", req.query.name)
+    resp.send("Hello " + req.query.name);
 });
 app.get('/about', (req, resp)=>{
     resp.send("Hello World from about");
